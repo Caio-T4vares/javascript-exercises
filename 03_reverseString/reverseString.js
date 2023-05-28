2f756f951e0a1dcae41f9744d45847c7c541d7af
Caio <102376017+Caio-T4vares@users.noreply.github.com>
2023-05-28 19:09:37 -0300

    comecar a responder

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const array = string.split("");
+  let reverseString = [];
+  for (let i = 0; i < array.length; i++) {
+    reverseString[i] = array[array.length - 1 - i];
+  }
+  return reverseString.join("");
 };
-
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...removeElements) {
+  return array.filter((element) => {
+    return !removeElements.includes(element);
+  });
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
