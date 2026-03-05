bac50b82a9f1eb159e047939f835a643f1f780e5
Caio-T4vares <caio.hosana@hotmail.com>
2026-03-05 15:12:19 -0300

    Resolve 09_sumAll exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (first, second) {
+  if (first < 0 || second < 0) return "ERROR";
+  if (!(Number.isInteger(first) && Number.isInteger(second))) return "ERROR";
+  if (!(typeof first === "number" && typeof second === "number"))
+    return "ERROR";
 
+  let sum = 0;
+  let initial = 0;
+  let final = 0;
+  if (first > second) {
+    initial = second;
+    final = first;
+  } else {
+    initial = first;
+    final = second;
+  }
+
+  for (let i = initial; i <= final; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
