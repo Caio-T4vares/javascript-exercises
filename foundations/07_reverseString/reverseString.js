db04c60ada590cbb067402acb5aef4d02797d09a
Caio-T4vares <caio.hosana@hotmail.com>
2026-03-05 14:53:50 -0300

    Resolve 07_reverseString exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  console.log(str);
+  return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+reverseString("caio");
