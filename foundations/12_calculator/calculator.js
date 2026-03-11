bde095ed34fe73dcfbeb39b86a642c0c9025a993
Caio-T4vares <caio.hosana@hotmail.com>
2026-03-11 11:29:29 -0300

    Resolve 12_calculator exercise

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((sum, el) => (sum += el), 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((sum, el) => (sum *= el), 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let result = a--;
+  while (a > 0) result *= a--;
+  return result === 0 ? 1 : result;
 };
 
 // Do not edit below this line
@@ -29,5 +31,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
