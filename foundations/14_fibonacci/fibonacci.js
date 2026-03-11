f112abe97c299021f30d03f2b7eff081023deb77
Caio-T4vares <caio.hosana@hotmail.com>
2026-03-11 13:45:35 -0300

    Resolve 14_fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) return "OOPS";
+  if (num == 0) return 0;
+  let actual = 1;
+  let prev = 0;
+  let cont = 1;
+  while (num > cont) {
+    const temp = actual;
+    actual += prev;
+    prev = temp;
+    cont++;
+  }
+  return actual;
 };
 
 // Do not edit below this line
