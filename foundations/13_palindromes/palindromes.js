305f982b91ba77fdb72bdeeb1c3483f8163ec023
Caio-T4vares <caio.hosana@hotmail.com>
2026-03-11 13:32:42 -0300

    Resolve 13_palindromes exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  const wordFilter = "abcdefghijklmnopqrstuvwxyz1234567890";
 
+  const reverse = word
+    .toLowerCase()
+    .split("")
+    .filter((char) => wordFilter.includes(char))
+    .reverse()
+    .join("");
+
+  const formatedWord = word
+    .toLowerCase()
+    .split("")
+    .filter((char) => wordFilter.includes(char))
+    .join("");
+  return reverse.toLowerCase() === formatedWord.toLowerCase();
 };
 
 // Do not edit below this line
